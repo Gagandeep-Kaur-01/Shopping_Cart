84517af8ccfabcaa1b33c8a15bcf7250a48dd3fd
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-30 19:14:50 +0530

    added more products

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -78,6 +78,25 @@ function Product() {
 
                 <div class="card">
                   <a href="product.html">         
+                    <img class="medium" src="./images/cc-4.jpg" alt="product" />
+                  </a>
+                  <div class="card-body">
+                    <a href="product.html">
+                      <h2>Cousin Covers</h2>
+                    </a>
+                    <div class="rating">
+                      <span> <i class="fa fa-star"></i> </span>
+                      <span> <i class="fa fa-star"></i> </span>
+                      <span> <i class="fa fa-star"></i> </span>
+                      <span> <i class="fa fa-star"></i> </span>
+                      <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">$120</div>
+                  </div>
+                </div>
+
+                <div class="card">
+                  <a href="product.html">         
                     <img class="medium" src="./images/sc-1.jpg" alt="product" />
                   </a>
                   <div class="card-body">
@@ -132,8 +151,30 @@ function Product() {
                       <div class="price">$120</div>
                     </div>
                 </div>
+
+                <div class="card">
+                    <a href="product.html">
+                      <img class="medium" src="./images/sc-4.jpg" alt="product" />
+                    </a>
+                    <div class="card-body">
+                      <a href="product.html">
+                        <h2>Sofa Covers</h2>
+                      </a>
+                     <div class="rating">
+                       <span> <i class="fa fa-star"></i> </span>
+                       <span> <i class="fa fa-star"></i> </span>
+                       <span> <i class="fa fa-star"></i> </span>
+                       <span> <i class="fa fa-star"></i> </span>
+                       <span> <i class="fa fa-star-half-o"></i> </span>
+                      </div>
+                      <div class="price">$120</div>
+                    </div>
+                </div>    
+                
              </div>
             </div>
+
+            
 
         </main>
         <footer class="row center">All right reserved</footer>
