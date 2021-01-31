3bbc0c46576085fefb19d6df5ca6dd7a1a699f65
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-31 19:17:36 +0530

    added Route(s)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Product from "./components/Products"
 
 function App() {
   return (
+    <BrowserRouter>
+    <Route path="/product/:id" component={ProductScreen}></Route>
+    <Route path="/" component={HomeScreen} exact></Route>
     <div> 
       <Product />
     </div>
+    </BrowserRouter>
   );
 }
 
