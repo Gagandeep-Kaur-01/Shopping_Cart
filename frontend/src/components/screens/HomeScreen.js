6b497161a9bda696d0789ddf56b4e0664eeaca6e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-31 19:25:33 +0530

    updated component

--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Product from '../Products';
 
 export default function HomeScreen() {
 
     return(
-        <div>
-            Home Screen
+        <div> 
+          <Product />
         </div>
     )
 }
