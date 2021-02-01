7cddef4ba45f8da08bb9c2bc20eee0cac1d08b06
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-01 12:51:00 +0530

    created server using node & express

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send("Server is ready");
 })
 
+// created very simple server using node & express
 app.listen(5000, () => {
     console.log('Serve at http://localhost:5000');
 });
+
