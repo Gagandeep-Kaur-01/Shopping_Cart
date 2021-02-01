4baaf9ecf26c0dd3ff6d32f953c865196f3c72f5
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-01 10:54:36 +0530

    working with the action col such as 'Add to cart'

--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -17,7 +17,7 @@ export default function ProductScreen(props) {
     // if product exists
     return(
         <div>
-           <div className="row">
+           <div className="row top">
                <div className="col-2">
                    <img className="large" src={product.image} alt={product.name}></img>
                </div>
@@ -42,9 +42,35 @@ export default function ProductScreen(props) {
                    </ul>
 
                </div>
+               {/* Action col */}
                <div className="col-1">
-
+                  <div className="card card-body">
+                    <ul>
+                      <li>
+                        <div className="row">
+                          <div>Price</div>
+                          <div className="price">${product.price}</div>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="row">
+                          <div>Status</div>
+                          <div>
+                            {product.countInStock > 0 ? (
+                              <span className="success">In Stock</span>
+                            ) : (
+                              <span className="error">Unavailable</span>
+                            )}
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <button className="primary block">Add to Cart</button>
+                      </li>
+                    </ul>
+                  </div>
                </div>
+
            </div>
         </div>
     )
