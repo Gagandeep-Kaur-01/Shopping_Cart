3dc96bd06373f9947aed4a2b4edd44bb2993639b
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-01 13:13:54 +0530

    returned/ displayed products

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import data from './data.js'
 
 const app = express();
 
+//return products in server.js
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
 app.get('/', (req, res) => {
     res.send("Server is ready");
-})
+});
 
 // created very simple server using node & express
 app.listen(5000, () => {
     console.log('Serve at http://localhost:5000');
 });
+
 
