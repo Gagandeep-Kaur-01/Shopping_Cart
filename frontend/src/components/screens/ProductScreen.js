9151bcccb47d14a890cd57963599962fe92c8dd1
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-01 10:34:22 +0530

    getting the details of the product

--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import Product from '../Products';
+import data from '../../data';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const product = data.products.find(x => x._id === props.match.params.id);
+    // props.match.params.id ==> this value is the value which user enter in the route or user select the product
 
+    // if product doesn't exists
+    if(!product) {
+        return 
+          <div>
+            Product not found :)
+          </div>
+    } 
+    // if product exists
     return(
         <div>
            <div className="row">
                <div className="col-2">
-
+                   <img className="large" src={product.image} alt={product.name}></img>
                </div>
                <div className="col-1">
 
                </div>
                <div className="col-1">
-                   
+
                </div>
            </div>
         </div>
